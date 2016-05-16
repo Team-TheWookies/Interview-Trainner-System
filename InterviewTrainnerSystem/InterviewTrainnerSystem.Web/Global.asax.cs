9788c5857c29b579f666abcc26675acbba414d80
Aleksandyr <aleksandyr.kamenov@gmail.com>
2016-05-16 23:41:16 +0300

    start AutoMapper in Global.asax

--- InterviewTrainnerSystem/InterviewTrainnerSystem.Web/Global.asax.cs
+++ InterviewTrainnerSystem/InterviewTrainnerSystem.Web/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Snippets.Common.Mappings;
 
 namespace InterviewTrainnerSystem.Web
 {
@@ -16,6 +18,12 @@ namespace InterviewTrainnerSystem.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
+            var autoMapperConfig = new AutoMapperConfig(new List<Assembly> { Assembly.GetExecutingAssembly() });
+            autoMapperConfig.Execute();
         }
     }
 }
